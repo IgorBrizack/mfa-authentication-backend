03fe2b1e915804b561bb2a6b9d8ca23935f82438
Igor Brizack <igor_brizack@hotmail.com>
2024-11-01 22:54:17 -0300

    feat: repository

---
+++ src/repository/user.repository.ts
@@ -0,0 +1,22 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../db/connection";
+import { IUserCreationAttributes, UserEntity } from "../entity/user.entity";
+import { IUser } from "../interfaces";
+
+export class UserRepository {
+  private userRepository: Repository<UserEntity>;
+  constructor() {
+    this.userRepository = AppDataSource.getRepository(UserEntity);
+  }
+
+  public async createUser(userData: IUserCreationAttributes): Promise<IUser> {
+    const userInstance = this.userRepository.create(userData);
+
+    return await this.userRepository.save(userInstance);
+  }
+
+  public async findByEmail(email: string): Promise<IUser | null> {
+    const user = await this.userRepository.findOneBy({ email });
+    return user;
+  }
+}
