ac4bdf8ce45df0fe3a6d060821a1b6194b0b0351
Igor Brizack <igor_brizack@hotmail.com>
2024-11-03 17:54:31 -0300

    feat: login route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
 import { Router, Request, Response } from "express";
 import { Validation } from "../validations";
-import { UserRegisterCommand } from "../use-cases/userRegister.command";
 import { UserRepository } from "../repository/user.repository";
 import { BaseError } from "../errors";
+import { LoginCommand, UserRegisterCommand } from "../use-cases";
 
 export class AppRouter {
   private userRepository: UserRepository;
@@ -24,6 +24,23 @@ export class AppRouter {
         new Validation().validateUserCreation(body);
 
         const command = new UserRegisterCommand(this.userRepository);
+        const result = await command.execute(body);
+
+        res.status(200).send(result);
+      } catch (error) {
+        if (error instanceof BaseError) {
+          res.status(error.statusCode).send({ error: error.message });
+        }
+        res.status(500);
+      }
+    });
+
+    router.post("/login", async (req: Request, res: Response) => {
+      const { body } = req;
+      try {
+        new Validation().validateUserLogin(body);
+
+        const command = new LoginCommand(this.userRepository);
         const result = await command.execute(body);
 
         res.status(200).send(result);
