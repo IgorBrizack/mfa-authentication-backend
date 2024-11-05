eb8bf998a94df6774640fefa402ca5cbe00bdd4d
Igor Brizack <igor_brizack@hotmail.com>
2024-11-05 20:51:32 -0300

    feat: middleware to validate if user is allowed

---
+++ src/middlewares/index.ts
@@ -0,0 +1,1 @@
+export { verifyMfa } from "./verifyMfa.middleware";
---
+++ src/middlewares/verifyMfa.middleware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { StatusCode } from "../enums";
+import { IUserJwt } from "../interfaces";
+import { HttpExceptionError } from "../errors";
+
+export const verifyMfa = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers["authorization"] as string; // Assume que o token é passado no header "Authorization"
+
+  if (!token) {
+    res.status(401).send({ error: "Token not provided" });
+  }
+
+  try {
+    const secretKey = process.env.JWT_SECRET || "your_secret_key";
+
+    const decoded = jwt.verify(token, secretKey) as IUserJwt;
+
+    if (!decoded.mfa_authentication.mfa_approved) {
+      throw new HttpExceptionError(
+        "User not authorized",
+        StatusCode.UNAUTHORIZED
+      );
+    }
+
+    next();
+  } catch (error) {
+    if (error instanceof HttpExceptionError) {
+      res.status(error.statusCode).send({ error: error.message });
+    }
+    res.status(500);
+  }
+};
