b3ccff1fad858a161c4f4b42786cf204d64d5ac0
Igor Brizack <igor_brizack@hotmail.com>
2024-11-20 13:41:04 -0300

    chore: remove url

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const prodDataSource = new DataSource({
   type: "postgres",
-  url: "postgresql://mfadb_owner:SYyHs53bpzvR@ep-white-bush-a53l1a91.us-east-2.aws.neon.tech/mfadb",
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: true,
   entities: [UserEntity],
