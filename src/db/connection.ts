0907e1b0efe5df1cf6bf8e9cfb0216022f2ff010
Igor Brizack <igor_brizack@hotmail.com>
2024-11-20 12:09:45 -0300

    feat: set prod data source

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -5,21 +5,39 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const prodDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "db", // "db" se refere ao container Docker
-  port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "rootpass",
-  database: process.env.DB_NAME || "mydatabase",
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
-  entities: [UserEntity], // Adicione suas entidades aqui
+  entities: [UserEntity],
   subscribers: [],
   migrations: ["./src/db/migrations/**/*{.ts,.js}"],
   extra: {
     connectionLimit: 10,
-    ssl: true,
-    allowPublicKeyRetrieval: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
+
+export const AppDataSource = process.env.PROD
+  ? prodDataSource
+  : new DataSource({
+      type: "postgres",
+      host: process.env.DB_HOST || "db", // "db" se refere ao container Docker
+      port: parseInt(process.env.DB_PORT || "5432"),
+      username: process.env.DB_USER || "root",
+      password: process.env.DB_PASS || "rootpass",
+      database: process.env.DB_NAME || "mydatabase",
+      synchronize: true,
+      logging: false,
+      entities: [UserEntity], // Adicione suas entidades aqui
+      subscribers: [],
+      migrations: ["./src/db/migrations/**/*{.ts,.js}"],
+      extra: {
+        connectionLimit: 10,
+        ssl: true,
+        allowPublicKeyRetrieval: true,
+      },
+    });
