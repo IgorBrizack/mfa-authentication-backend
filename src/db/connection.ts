38a88a9a3915f3af7a7e6e49d97c534e151f54dd
Igor Brizack <igor_brizack@hotmail.com>
2024-11-20 11:46:45 -0300

    feat: default postgress port

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -8,7 +8,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST || "db", // "db" se refere ao container Docker
-  port: parseInt(process.env.DB_PORT || "3306"),
+  port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "rootpass",
   database: process.env.DB_NAME || "mydatabase",
